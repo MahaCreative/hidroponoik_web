a431480aacddbbaa7593e6c5682f0c9be8d78399
MahaCreative <gunturmadjid.3@gmail.com>
2023-08-09 01:12:19 -0700

    b

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('getData', function(){
     $data = DataIot::first();
-    dd($data);
+    return $data;
 })->name('getData');
 
 Route::post('postData', function(Request $request){
